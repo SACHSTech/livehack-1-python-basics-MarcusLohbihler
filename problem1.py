d20b6ee95e27175b7226c2f7bacd5b2bb3c42484
Marcus Lohbihler <marcus.lohbihler23@ycdsbk12.ca>
2020-12-07 18:46:19 +0000

    Completed livehack challenges

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+print ("********** Welcome to Celsius to Fahrenheit temperature converter **********")
 
+celsius = float(input("Insert Temperature in Celsius here "))
+
+
+
+
+
+fahrenheit = (celsius * 9/5 + 32)
+
+
+
+
+
+
+print ("The temperature in Fahrenheit is " + str(fahrenheit))
+print ("Thank you for using the Celsius to Fahrenheit temperature converter.")
+print("****************************************************************************")
